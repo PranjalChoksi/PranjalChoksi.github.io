9e906e985b07c11ff79cc7f3e647d2f35a2e1f2a
Pranjal Choksi <150175368+PranjalChoksi@users.noreply.github.com>
2023-11-14 23:27:00 +0530

    week4

---
+++ week4/script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
+
+for(var i = 0; i<name.length;i++){
+    if(names[1].charAt(0)==='J' || name[i].charAt==='j'){
+        console.log("GoodBye "+ name[i])
+    }
+    else{
+        console.log("Hello "+ name[i])
+    }
+}
